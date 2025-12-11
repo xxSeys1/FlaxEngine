9e469d48c98e0ce2e6890f4e6d8d2750fcc35114
Saas <kaktusreiniger@gmail.com>
2025-12-11 21:14:56 +0100

    Revert "Merge branch 'PressGToGameModeAndPToNavigate' into Flax_DroneGame"
    This reverts commit e1c90dd2ff73b1d88dc997ddf951239451b19535, reversing
    changes made to 3ee401f7c3a8f02e36cd61b2f75dc75c4e357143.

--- Source/Editor/Viewport/MainEditorGizmoViewport.cs
+++ Source/Editor/Viewport/MainEditorGizmoViewport.cs
@@ -506,7 +506,6 @@ namespace FlaxEditor.Viewport
         {
             if (!_gameViewActive)
             {
-                // Cache flags & values
                 _preGameViewFlags = Task.ViewFlags;
                 _preGameViewViewMode = Task.ViewMode;
                 _gameViewWasGridShown = Grid.Enabled;
@@ -514,12 +513,11 @@ namespace FlaxEditor.Viewport
                 _gameViewWasNagivationShown = ShowNavigation;
             }
 
-            // Set flags & values
             Task.ViewFlags = _gameViewActive ? _preGameViewFlags : ViewFlags.DefaultGame;
             Task.ViewMode = _gameViewActive ? _preGameViewViewMode : ViewMode.Default;
-            ShowFpsCounter = _gameViewActive ? _gameViewWasFpsCounterShown : false;
-            ShowNavigation = _gameViewActive ? _gameViewWasNagivationShown : false;
-            Grid.Enabled = _gameViewActive ? _gameViewWasGridShown : false;
+            ShowFpsCounter = _gameViewActive ? _gameViewWasGridShown : false;
+            ShowNavigation = _gameViewActive ? _gameViewWasFpsCounterShown : false;
+            Grid.Enabled = _gameViewActive ? _gameViewWasNagivationShown : false;
 
             _gameViewActive = !_gameViewActive;
 
